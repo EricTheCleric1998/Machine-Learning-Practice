11a18581770900bfa3c6ede6f2b632bc3257150c
Joel Castellanos <joel@stellarscience.com>
2024-06-17 20:35:10 -0600

    added an example csv file and reader

---
+++ src/fish_data_reader.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+# The current directory is the src directory of the project. Go up one level (..). Then look in the data directory.
+path = r'..\data\fish_data.csv'
+
+# To help with understanding, print the absolute path
+print(os.path.abspath(path))
+
+# This one line reads the whole data file into a 2D array of 32-bit floats (skipping the header row).
+data = np.genfromtxt(path, delimiter=',', dtype=np.float32, skip_header=1)
+
+# The : specifies a whole column of the 2D array, so these 2 prints shows the three columns
+print(data[:, 0])
+print(data[:, 1])
+print(data[:, 2])
+
+fig3 = plt.figure(1)
+
+# Set up a color table so that 0=Skipjack Tuna is blue and 1=Atlantic Salmon is salmon.
+my_colortable = ['blue', 'salmon']
+
+# Create the colormap
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("blue_salmon", my_colortable)
+
+x = data[:, 0]  # Not necessary to use this temp variable, but makes the scatter function clearer
+y = data[:, 1]
+color = data[:, 2].astype(int)
+
+
+plt.scatter(x, y, c=color, cmap=cmap, marker="*", s=50)
+plt.xlabel("Length (cm)")
+plt.ylabel("Weight (kg)")
+plt.grid(color='lightgray', linestyle='--', linewidth=1)
+plt.show()
+
+# This is a very fast and powerful way to index NumPy arrays
+true_if_tuna = data[:, 2] == 0
+print(true_if_tuna)
+tuna_length = data[true_if_tuna, 0]
+tuna_width = data[true_if_tuna, 1]
+print(tuna_length)
